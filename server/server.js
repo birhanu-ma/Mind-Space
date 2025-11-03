b82413536f0c01fb5c5b6d99eaab529720d2fd68
Birhanu <birhanumatebe12@gmail.com>
2025-11-03 18:02:26 +0300

    refactor: improve import

--- server/server.js
+++ server/server.js
@@ -1,7 +1,6 @@
 // file for server set up
-
 import express from "express";
-import mongoose from "mongoose"
+import mongoose from "mongoose";
 import app from "./app.js";
 import dotenv from "dotenv";
 import http from "http";
