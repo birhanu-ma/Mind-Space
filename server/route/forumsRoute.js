46e811f3667e668179a1ea6299dea76536268127
Birhanu <birhanumatebe12@gmail.com>
2025-11-17 15:45:55 +0300

    feat: add forumsRoute

---
+++ server/route/forumsRoute.js
@@ -0,0 +1,17 @@
+import express from "express";
+import { protect, restrictTo } from "../controller/authController.js";
+import { getForumsByType,createForum,updateForum,deleteForum, getAllForums } from "../controller/forumController.js";
+
+const router = express.Router();
+
+
+router
+  .route("/")
+  .post(protect, restrictTo("counselor", "admin"), createForum)
+  .get(protect, getAllForums)
+  .patch(protect, restrictTo("counselor", "admin"), updateForum)
+  .delete(protect, restrictTo("admin"), deleteForum);
+
+router.route("/by-type").get(protect, getForumsByType);
+
+export default router;
