6a199ca171ab4a0df9d0c9da9245d1f21591419f
Birhanu <birhanumatebe12@gmail.com>
2025-11-17 15:52:35 +0300

    refactor: improve counselingRoute

--- server/route/counselingRoute.js
+++ server/route/counselingRoute.js
@@ -4,15 +4,15 @@ import {
   createCounseling,
   deleteCounseling,
   getCounseling,
-  updateCounseling,
-} from "../controller/counsellingController.js";
+  
+} from "../controller/counselingController.js";
 const router = express.Router();
 
 router
   .route("/")
   .post(protect, restrictTo("admin"), createCounseling)
   .get(protect, restrictTo("mentee", "counselor", "admin"), getCounseling)
-  .patch(protect, restrictTo("admin"), updateCounseling)
+
   .delete(protect, restrictTo("admin"), deleteCounseling);
 
 export default router;
