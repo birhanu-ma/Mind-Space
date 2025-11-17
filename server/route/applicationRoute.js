6408368afe84548fdddb40fc5003784192f81f70
Birhanu <birhanumatebe12@gmail.com>
2025-11-17 15:51:52 +0300

    refactor: improve applicationRoute

--- server/route/applicationRoute.js
+++ server/route/applicationRoute.js
@@ -1,7 +1,17 @@
 import express from "express";
+import { protect, restrictTo } from "../controller/authController.js";
+import { getApplicationsByType,createApplication,updateApplication,deleteApplication, getAllApplications } from "..//controller/ApplicationController.js";
 
 const router = express.Router();
 
-router.route("/");
+
+router
+  .route("/")
+  .post(protect, restrictTo( "admin","counselor"), createApplication)
+  .get(protect, getAllApplications)
+  .patch(protect, restrictTo( "admin"), updateApplication)
+  .delete(protect, restrictTo("admin"), deleteApplication);
+
+router.route("/by-type").get(protect, getApplicationsByType);
 
 export default router;
