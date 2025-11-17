bfbe2f6a809fd078c07ec610666b8ca709593ce9
Birhanu <birhanumatebe12@gmail.com>
2025-11-17 15:44:22 +0300

    feat: add professionModel

---
+++ server/model/professionalModel.js
@@ -0,0 +1,57 @@
+import mongoose from "mongoose";
+
+const professionalSchema = new mongoose.Schema(
+  {
+    user: {
+      type: String,
+    //   ref: "User",
+      required: true, 
+    },
+
+    profession: {
+      type: String,
+      required: [true, "Profession is required"], 
+    },
+
+    specialization: {
+      type: [String],
+      default: [], 
+    },
+
+    experienceYears: {
+      type: Number,
+      default: 0,
+    },
+
+    servicesOffered: {
+      type: [String],
+      default: [],
+    },
+
+    profileImage: {
+      type: String, 
+      default: "",
+    },
+
+    rating: {
+      type: Number,
+      default: 0,
+      min: 0,
+      max: 5,
+    },
+
+    verified: {
+      type: Boolean,
+      default: false,
+    },
+
+    active: {
+      type: Boolean,
+      default: true,
+    },
+  },
+  { timestamps: true }
+);
+
+const Professional = mongoose.model("Professional", professionalSchema);
+export default Professional;
