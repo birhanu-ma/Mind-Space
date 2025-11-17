9ca526f2a87b22be4ec34ba948c3603d9c5f68cd
Birhanu <birhanumatebe12@gmail.com>
2025-11-17 15:49:55 +0300

    refactore: improve applicationController

--- server/controller/applicationController.js
+++ server/controller/applicationController.js
@@ -1,10 +1,42 @@
-import Application from "../model/applicationModel.js";
+import Application from "../model/ApplicationModel.js";
 import * as factory from "./handlerFactory.js";
-
+import APIFeatures from "../utils/apiFeatures.js";
 export const createApplication = factory.createOne(Application);
 export const getApplication = factory.getOne(Application, {
-  path: "counselor mentee reviewedBy",
-  select: "name  role",
+  path: "likes dislikes comments reviewedBy relatedApplications",
+  select: "name role",
 });
+export const getAllApplications = factory.getAll(Application);
 export const updateApplication = factory.updateOne(Application);
 export const deleteApplication = factory.deleteOne(Application);
+
+export const getApplicationsByType = async (req, res, next) => {
+  try {
+    console.log("Application query", req.query);
+    const { applicationType, q, ...restQuery } = req.query;
+    const baseQuery = applicationType ? Application.find({ applicationType }) : Application.find();
+
+    const features = new APIFeatures(baseQuery, restQuery)
+      .filter()
+      .sort()
+      .limitFields()
+      .paginate();
+
+    if (q) {
+      const regex = new RegExp(q, "i");
+      features.query = features.query.find({
+        $or: [{ name: regex }, { sims_id: regex }],
+      });
+    }
+
+    const Applications = await features.query;
+
+    res.status(200).json({
+      status: "success",
+      results: Applications.length,
+      data: Applications,
+    });
+  } catch (err) {
+    next(err);
+  }
+};
