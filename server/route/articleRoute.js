ed5f2e6d10fe76d3e4f54b800e7b15107779a06e
Birhanu <birhanumatebe12@gmail.com>
2025-11-17 15:52:16 +0300

    refactor: improve articleRoute

--- server/route/articleRoute.js
+++ server/route/articleRoute.js
@@ -3,7 +3,8 @@ import { protect, restrictTo } from "../controller/authController.js";
 import {
   createArticle,
   deleteArticle,
-  getArticle,
+  getAllArticles,
+  getArticlesByType,
   updateArticle,
 } from "../controller/articleController.js";
 const router = express.Router();
@@ -11,8 +12,10 @@ const router = express.Router();
 router
   .route("/")
   .post(protect, restrictTo("counselor", "admin"), createArticle)
-  .get(protect, getArticle)
+  .get(protect, getAllArticles)
   .patch(protect, restrictTo("counselor", "admin"), updateArticle)
   .delete(protect, restrictTo("admin"), deleteArticle);
+
+router.route("/by-type").get(protect, getArticlesByType);
 
 export default router;
