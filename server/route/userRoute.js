dcbaeda584655281e6e338d185ae1f1fd13b6bd9
Birhanu <birhanumatebe12@gmail.com>
2025-11-17 15:52:51 +0300

    refactor: improve userRoute

--- server/route/userRoute.js
+++ server/route/userRoute.js
@@ -1,6 +1,15 @@
 import express from "express";
+import { protect, restrictTo } from "../controller/authController.js";
+import { getUsersByRole, getAllUsers } from "../controller/usersController.js";
 const router = express.Router();
 
-router.route("/");
+router.route("/").get(protect, restrictTo("admin"), getAllUsers);
+
+router.get(
+  "/by-role",
+  protect,
+  restrictTo("student-union", "mentor", "admin"),
+  getUsersByRole
+);
 
 export default router;
