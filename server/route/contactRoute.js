1f90bffeb668d41a1564c0ff74d4de3509c20827
Birhanu <birhanumatebe12@gmail.com>
2025-12-17 14:08:29 +0300

    feat: add role access

--- server/route/contactRoute.js
+++ server/route/contactRoute.js
@@ -1,16 +1,21 @@
 import express from "express";
 import { protect, restrictTo } from "../controller/authController.js";
-import { createMessage, deleteMessage, getAllMessage, getMessage } from "../controller/messageController.js";
+import {
+  createMessage,
+  deleteMessage,
+  getAllMessage,
+  getMessage,
+} from "../controller/messageController.js";
 const router = express.Router();
 
 router
   .route("/")
   .get(protect, restrictTo("admin"), getAllMessage)
-  .post(protect, restrictTo("counselor","mentee","user"), createMessage)
+  .post(protect, restrictTo("counselor", "mentee", "user"), createMessage);
 
 router
   .route("/:id")
-  .get(protect, restrictTo("admin"), getMessage)
-  .delete(protect, restrictTo("admin"), deleteMessage)
+  .get(protect, restrictTo("admin", "mentee", "user","counselor"), getMessage)
+  .delete(protect, restrictTo("admin", "mentee", "user","counselor"), deleteMessage);
 
-export default router
+export default router;
