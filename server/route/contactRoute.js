ee3a15a624d9d3785fd28edc3bea6ecf699f0f08
Birhanu <birhanumatebe12@gmail.com>
2025-12-17 14:05:59 +0300

    feat: add dynamic route

--- server/route/contactRoute.js
+++ server/route/contactRoute.js
@@ -1,12 +1,16 @@
 import express from "express";
 import { protect, restrictTo } from "../controller/authController.js";
-import { createMessage, deleteMessage, getAllMessage } from "../controller/messageController.js";
+import { createMessage, deleteMessage, getAllMessage, getMessage } from "../controller/messageController.js";
 const router = express.Router();
 
 router
   .route("/")
   .get(protect, restrictTo("admin"), getAllMessage)
   .post(protect, restrictTo("counselor","mentee","user"), createMessage)
+
+router
+  .route("/:id")
+  .get(protect, restrictTo("admin"), getMessage)
   .delete(protect, restrictTo("admin"), deleteMessage)
 
 export default router
