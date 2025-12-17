ebe72b316fb7b4612900843296c3979f69d7e0a2
Birhanu <birhanumatebe12@gmail.com>
2025-12-17 14:03:30 +0300

    feat: add contactRoute.js

---
+++ server/route/contactRoute.js
@@ -0,0 +1,12 @@
+import express from "express";
+import { protect, restrictTo } from "../controller/authController.js";
+import { createMessage, deleteMessage, getAllMessage } from "../controller/messageController.js";
+const router = express.Router();
+
+router
+  .route("/")
+  .get(protect, restrictTo("admin"), getAllMessage)
+  .post(protect, restrictTo("counselor","mentee","user"), createMessage)
+  .delete(protect, restrictTo("admin"), deleteMessage)
+
+export default router
