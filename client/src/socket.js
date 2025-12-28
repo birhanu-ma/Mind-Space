bb74fa8e86652cf06d95cc762d2d1cebdf49bf09
Birhanu <birhanumatebe12@gmail.com>
2025-12-28 17:15:20 +0300

    refactor: chat root url changed

--- client/src/socket.js
+++ client/src/socket.js
@@ -1,7 +1,7 @@
 // src/socket.js
 import { io } from "socket.io-client";
 
-export const socket = io("http://localhost:5000", {
+export const socket = io("https://mind-space-atfn.onrender.com", {
   transports: ["websocket"], 
   withCredentials: true,
   autoConnect: false,       
